7f7b923b270f7eed3a13ba8fa9367d87571d8622
Philippe Daouadi <philippe.daouadi@metalgear.xyz>
2024-06-03 16:10:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Caching.Hybrid;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHybridCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.ConfigurationOptions = new()
+        {
+            EndPoints = [],
+            Password = "p4ssw0rd"
+        };
+
+        options.ConfigurationOptions.EndPoints.Add("localhost:6379");
+    });
+
+var app = builder.Build();
+
+var hybridCache = app.Services.GetRequiredService<HybridCache>();
+
+Console.WriteLine("Will get");
+await hybridCache.GetOrCreateAsync(args[0], async (_) =>
+{
+    Console.WriteLine("Creating");
+    await Task.Delay(TimeSpan.FromHours(1));
+    return "ok";
+});
